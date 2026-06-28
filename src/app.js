3776463eb4ed05e260cf3f72e601e7fd055a5c00
badri95699 <badri.gupta.work@gmail.com>
2026-06-28 14:00:10 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Server started.. listening on port 3000");
+})
+
+app.use("/", (req,res) => {
+    res.send("hello World from server!!")
+})
+app.use("/test", (req,res) => {
+    res.send("Test page")
+})
