5fe14ecbf0271c653bdb092066a957442a48f925
badri95699 <badri.gupta.work@gmail.com>
2026-07-28 01:16:08 +0530

    small changes for ui

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -9,11 +17,12 @@ app.use(cookieParser());
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
-const userRouter = require("./routes/user")
+const userRouter = require("./routes/user");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
+app.use("/", userRouter);
 
 connectDB()
   .then(() => {
--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -4,29 +4,41 @@ const connectionRequestSchema = new mongoose.Schema(
   {
     fromUserId: {
       type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: true,
     },
     toUserId: {
       type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: true,
     },
     status: {
       type: String,
       required: true,
       enum: {
-        values: ["ignored", "interested", "accepeted", "rejected"],
+        values: ["ignored", "interested", "accepted", "rejected"],
         message: `{VALUE} is incorrect status type`,
       },
     },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
+// ConnectionRequest.find({fromUserId: 273478465864786587, toUserId: 273478465864786587})
 
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
 
-const ConnectionRequestModel = new mongoose.model(
+connectionRequestSchema.pre("save", function () {
+  const connectionRequest = this;
+  // Check if the fromUserId is same as toUserId
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("Cannot send connection request to yourself!");
+  }
+});
+
+const ConnectionRequestModel = mongoose.model(
   "ConnectionRequest",
-  connectionRequestSchema
+  connectionRequestSchema,
 );
 
 module.exports = ConnectionRequestModel;
